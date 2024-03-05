fb00542e6b424fe3b66d064680d4ab41a7850b4c
Raja726626 <161327701+Raja726626@users.noreply.github.com>
2024-03-05 17:40:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['917605902011','UCHIHA OBITO', true],
+  ['6281255369012', 'UCHIHA OBITO', true], 
   [''] 
 ] //Number of owners
 
